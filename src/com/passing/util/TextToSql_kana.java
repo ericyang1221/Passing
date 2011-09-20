7a3603999730e2cbe1db5dcbfde91fcdd72e5c2c
= <=>
2011-09-20 22:10:38 +0800

    Use relative path to find orginal text file.

--- src/com/passing/util/TextToSql_kana.java
+++ src/com/passing/util/TextToSql_kana.java
@@ -59,29 +59,29 @@ public class TextToSql_kana {
 				if (currentKanaStr.charAt(0) == '‐') {
 					currentKanaStr = currentKanaStr.substring(1);
 				}
-				if(currentKanaStr.charAt(currentKanaStr.length()-1) > '0' && currentKanaStr.charAt(currentKanaStr.length()-1) <= '9'){
-					currentKanaStr = currentKanaStr.substring(0, currentKanaStr.length()-1);
+				if (currentKanaStr.charAt(currentKanaStr.length() - 1) > '0'
+						&& currentKanaStr.charAt(currentKanaStr.length() - 1) <= '9') {
+					currentKanaStr = currentKanaStr.substring(0,
+							currentKanaStr.length() - 1);
 				}
 				try {
 					if (br.ready()) {
 						readbuffer = br.readLine();
 						if (readbuffer.indexOf("\t[trn]") > 0) {
 							readbuffer = readbuffer.substring(readbuffer
-									.indexOf("\t[trn]")
-									+ "\t[trn]".length());
+									.indexOf("\t[trn]") + "\t[trn]".length());
 							readbuffer = "\t" + readbuffer;
 						} else if (readbuffer.indexOf("[trn]") > 0
 								&& !readbuffer.endsWith("[trn]")) {
 							readbuffer = readbuffer.substring(readbuffer
-									.indexOf("[trn]")
-									+ "[trn]".length());
+									.indexOf("[trn]") + "[trn]".length());
 							readbuffer = "\t" + readbuffer;
 						}
 						if (readbuffer.indexOf('【') != -1) {
 							if (readbuffer.indexOf('【') != -1) {
 								currentWordStr = readbuffer.substring(
-										readbuffer.indexOf('【') + 1, readbuffer
-												.indexOf('】'));
+										readbuffer.indexOf('【') + 1,
+										readbuffer.indexOf('】'));
 							}
 							writebuffer = "insert into JPWORD values('"
 									+ wordrow + "','" + currentKanaStr + "','"
@@ -170,21 +170,22 @@ public class TextToSql_kana {
 				}
 				doExample();
 				meaningrow++;
-			} 
+			}
 		}
 	}
 
 	public void doExample() throws Exception {
 		try {
 			while ("\t[*]".equals(readbuffer.substring(0, 4))) {
-				currentExampleStr = readbuffer.substring(4, readbuffer
-						.indexOf('／'));
-				currentExampleMeaningStr = readbuffer.substring(readbuffer
-						.indexOf('／') + 1, readbuffer.indexOf("[/*]"));
+				currentExampleStr = readbuffer.substring(4,
+						readbuffer.indexOf('／'));
+				currentExampleMeaningStr = readbuffer
+						.substring(readbuffer.indexOf('／') + 1,
+								readbuffer.indexOf("[/*]"));
 				if (currentExampleMeaningStr.indexOf("[/trn]") > 0) {
 					currentExampleMeaningStr = currentExampleMeaningStr
-							.substring(0, currentExampleMeaningStr
-									.indexOf("[/trn]"));
+							.substring(0,
+									currentExampleMeaningStr.indexOf("[/trn]"));
 				}
 				writebuffer = "insert into JPWORDEXAMPLE values('" + examplerow
 						+ "','" + meaningrow + "','" + currentExampleStr
@@ -209,24 +210,32 @@ public class TextToSql_kana {
 				}
 			}
 			if ("\t『語法』".equals(readbuffer.substring(0, "\t『語法』".length()))
-					|| "\t『比較』".equals(readbuffer.substring(0, "\t『比較』".length()))
+					|| "\t『比較』".equals(readbuffer.substring(0,
+							"\t『比較』".length()))
 					|| "\t★".equals(readbuffer.substring(0, "\t★".length()))
-					|| "\t『参考』".equals(readbuffer.substring(0, "\t『参考』".length()))
+					|| "\t『参考』".equals(readbuffer.substring(0,
+							"\t『参考』".length()))
 					|| "\t▼".equals(readbuffer.substring(0, "\t▼".length()))) {
 				String remarkStr = readbuffer;
 				readbuffer = br.readLine();
-				while(readbuffer.charAt(0) == '\t' && readbuffer.charAt(1) != '（' && !"[*]".equals(readbuffer.substring(1,"[*]".length()))){
+				while (readbuffer.charAt(0) == '\t'
+						&& readbuffer.charAt(1) != '（'
+						&& !"[*]"
+								.equals(readbuffer.substring(1, "[*]".length()))) {
 					remarkStr += readbuffer;
 					readbuffer = br.readLine();
 				}
 				remarkStr = remarkStr.substring(1);
-				if(remarkStr.indexOf("[/trn]") > 0){
-					remarkStr = remarkStr.substring(0, remarkStr.indexOf("[/trn]"));
+				if (remarkStr.indexOf("[/trn]") > 0) {
+					remarkStr = remarkStr.substring(0,
+							remarkStr.indexOf("[/trn]"));
 				}
-				if(remarkStr.indexOf("[/*]") > 0){
-					remarkStr = remarkStr.substring(0, remarkStr.indexOf("[/*]"));
+				if (remarkStr.indexOf("[/*]") > 0) {
+					remarkStr = remarkStr.substring(0,
+							remarkStr.indexOf("[/*]"));
 				}
-				writebuffer = "insert into JPWORDREMARK values('"+remarkrow+"','"+wordrow+"','"+remarkStr+"');";
+				writebuffer = "insert into JPWORDREMARK values('" + remarkrow
+						+ "','" + wordrow + "','" + remarkStr + "');";
 				System.out.println(writebuffer);
 				bw.write(writebuffer);
 				bw.newLine();
@@ -238,8 +247,9 @@ public class TextToSql_kana {
 	}
 
 	public static void main(String[] args) {
-		String inputFilePath = "E:/kana_edited.txt";
-		String outputFilePath = "E:/output.sql";
+		String projectPath = System.getProperty("user.dir");
+		String inputFilePath = projectPath + "/dict/kana_edited.txt";
+		String outputFilePath = projectPath + "/dict/output.sql";
 		new TextToSql_kana().doTextToSql(inputFilePath, outputFilePath);
 	}
 
