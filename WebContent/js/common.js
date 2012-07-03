0300a6c13ed45e1ec21078cb40c6a46f9511c9af
lovelease <lovelease@gmail.com>
2012-07-03 23:39:13 +0800

    add common.js to simply import jquery UI plugin to every html file

--- WebContent/js/common.js
+++ WebContent/js/common.js
@@ -16,4 +16,30 @@ $(document).ready(function(){
 		$(this).css({background:"#EC3A58"});
 	});
 	
+	
+	//how to use the function below:
+	//$.include('file/ajaxa.js');$.include('file/ajaxa.css');
+	//or $.includePath  = 'file/';$.include(['ajaxa.js','ajaxa.css']);(only if .js and .css files are in the same directory)
+	$.extend({
+	    includePath: '',
+	    include: function(file)
+	    {
+	        var files = typeof file == "string" ? [file] : file;
+	        for (var i = 0; i < files.length; i++)
+	        {
+	            var name = files[i].replace(/^\s|\s$/g, "");
+	            var att = name.split('.');
+	            var ext = att[att.length - 1].toLowerCase();
+	            var isCSS = ext == "css";
+	            var tag = isCSS ? "link" : "script";
+	            var attr = isCSS ? " type='text/css' rel='stylesheet' " : " type='text/javascript' ";
+	            var link = (isCSS ? "href" : "src") + "='" + $.includePath + name + "'";
+	            if ($(tag + "[" + link + "]").length == 0) $("head").prepend("<" + tag + attr + link + "></" + tag + ">");
+	        }
+	    }
+	});
+	$.include('../js/jquery-ui-1.8.21.custom.min.js');
+	$.include('../css/black-tie/jquery-ui-1.8.21.custom.css');
+	
 });
+
