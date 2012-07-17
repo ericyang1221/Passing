a14fdda595397f770ef380a7806d93a51e8a85fd
lovelease <lovelease@gmail.com>
2012-07-17 23:42:55 +0800

    to change the way of data transport from using form to not using form

--- WebContent/js/login.js
+++ WebContent/js/login.js
@@ -1,17 +1,37 @@
 $(document).ready(function() {
 	$('.button').button();
-	// bind 'loginForm' and provide a simple callback function
-	var options = {
+	
+/************************************************** to transform data with form START *********************************************************
+	$('#loginForm').submit(function() {
+		var options = {
 				url:'../doLogin.do',
 				type:'POST',
 				dataType:'json',
 				success: loginResponse
 			};
-	
-	$('#loginForm').submit(function() {
 		$(this).ajaxSubmit(options); 
 		// return false to prevent normal browser submit and page navigation
 		return false;
+	});
+*************************************************** to transform data with form END ********************************************************/
+
+/************************************************** to transform data without form START*******************************************************/
+	$('input[name=submit]').click(function() {
+		var userInfo = "userName=" + $('input[name=userName]').fieldValue() + "&password=" + $('input[name=password]').val();//.fieldValue() and .val() can both getthe value
+		var options = {
+				url:'../doLogin.do',
+				type:'POST',
+				dataType:'json',
+				data:userInfo,
+				success: loginResponse
+			};
+		$.ajax(options); 
+	});
+/************************************************** to transform data without form END*******************************************************/
+
+	$('input[name=reset]').click(function() {
+		$('input[name=userName]').val("");
+		$('input[name=password]').val("");
 	});
 	
 	function loginResponse(result) { 
--- src/com/passing/struts/action/LoginAction.java
+++ src/com/passing/struts/action/LoginAction.java
@@ -3,6 +3,10 @@ package com.passing.struts.action;
 import static com.passing.consts.CommonConsts.COM_SPACE;
 
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -24,15 +28,20 @@ public class LoginAction extends Action {
 	private LoginServiceBean loginServiceBean;
 	private String encoding = "UTF-8";
 	private String contentType = "application/json";
-	
+
 	@Override
 	public ActionForward execute(ActionMapping mapping, ActionForm form,
 			HttpServletRequest request, HttpServletResponse response)
 			throws Exception {
-		LoginForm loginForm = (LoginForm) form;
-
-		String userName = loginForm.getUserName();
-		String password = loginForm.getPassword();
+		
+		/** when using form for data transform */
+//		LoginForm loginForm = (LoginForm) form;
+//		String userName = loginForm.getUserName();
+//		String password = loginForm.getPassword();
+		
+		/** when using ajax for data transform */
+		String userName = request.getParameter("userName");
+		String password = request.getParameter("password");
 
 		String loginRst = loginServiceBean.login(userName + COM_SPACE
 				+ password);
@@ -44,15 +53,37 @@ public class LoginAction extends Action {
 		JSONObject jsonObj = new JSONObject();
 		jsonObj.put("loginResult", loginRst);
 		
+		/** make a jsonObject with map */
+//		Map<String, Object> person1 = new HashMap<String, Object>();
+//		Map<String, String> person2 = new HashMap<String, String>();
+//		person1.put("age", "10");
+//		person1.put("sex", "male");
+//		
+//		List<String> habits = new ArrayList<String>();
+//		habits.add("basket ball");
+//		habits.add("foot ball");
+//		
+//		person1.put("habits", habits);
+//		person2.put("age2", "10");
+//		person2.put("sex2", "male");
+//		person2.put("habit2", "afda");
+		
+		/** make a jsonObject with bean_part1(its the best way) */
+//		List<Object> persons = new ArrayList<Object>();
+//		persons.add(new Person("name1",12,new String[]{"basketball","football"}));
+//		persons.add(new Person("name13",122,new String[]{"basketball222","football222"}));
+//		
+//		jsonObj.put("loginResult", persons);
+		
 		response.setContentType(contentType);
 		response.setCharacterEncoding(encoding);
 		PrintWriter pw = response.getWriter(); 
 		pw.write(jsonObj.toString());
 		pw.flush();
-//		response.getOutputStream().write(jsonObj.toString().getBytes(encoding));
 		
 		return mapping.findForward(null);
 		
+		/** actionForward must be returned when using form for data transform */
 //		if (LoginConsts.LOG_SUCCESS.equals(loginRst)) {
 //
 //			return (mapping.findForward("success"));
@@ -62,9 +93,47 @@ public class LoginAction extends Action {
 //			return (mapping.findForward("failure"));
 //		}
 		
-		
-		
 	}
+
+	/** make a jsonObject with bean_part2(its the best way) */
+//	public class Person {
+//		private String name;
+//		private int age;
+//		private String[] habats;
+//
+//		public Person() {
+//		}
+//
+//		public Person(String name, int age, String[] habits) {
+//			this.name = name;
+//			this.age = age;
+//			this.habats = habits;
+//		}
+//
+//		public String getName() {
+//			return name;
+//		}
+//
+//		public void setName(String name) {
+//			this.name = name;
+//		}
+//
+//		public int getAge() {
+//			return age;
+//		}
+//
+//		public void setAge(int age) {
+//			this.age = age;
+//		}
+//
+//		public String[] getHabats() {
+//			return habats;
+//		}
+//
+//		public void setHabats(String[] habats) {
+//			this.habats = habats;
+//		}
+//	}
 
 	public LoginServiceBean getLoginServiceBean() {
 		return loginServiceBean;
