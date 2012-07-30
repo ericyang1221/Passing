585a50815e1186d6ae76f3faccd8c1c13483bcec
lovelease <lovelease@gmail.com>
2012-07-30 16:31:27 +0800

    create BaseAction class to improve source structure

---
+++ src/com/passing/struts/action/BaseAction.java
@@ -0,0 +1,44 @@
+package com.passing.struts.action;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletResponse;
+
+import net.sf.json.JSONObject;
+
+import org.apache.struts.action.Action;
+
+import com.passing.util.LogUtil;
+
+/**
+ * @author weishijie
+ * BaseAction class which all action classes must extend
+ */
+public class BaseAction extends Action{
+
+	private String encoding = "UTF-8";
+	private String contentType = "application/json";
+	
+	/**
+	 * @param response response
+	 * @param objName jsonObjectName
+	 * @param obj jsonObject
+	 * @throws IOException
+	 */
+	protected void makeJSONObject(HttpServletResponse response, String objName, Object obj) throws IOException {
+
+		this.contentType = contentType + ";charset=" + encoding;
+		LogUtil.log.info("Set contentType to: " + contentType);
+		
+		JSONObject jsonObj = new JSONObject();
+		jsonObj.put(objName, obj);
+		
+		response.setContentType(contentType);
+		response.setCharacterEncoding(encoding);
+		PrintWriter pw = response.getWriter(); 
+		pw.write(jsonObj.toString());
+		pw.flush();
+		
+	}
+}
