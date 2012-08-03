dd8eead4eea5d067cf616300d68627d255ca05a6
lovelease <lovelease@gmail.com>
2012-08-03 18:04:58 +0800

    通过url实现不同页面的数据共用：验证OK

--- WebContent/js/testrstl.js
+++ WebContent/js/testrstl.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 
-	alert(r);
-	$('#test').val(r);
-	
+	//window.location can get the whole url which contains the data from test page.
+	var urlData = window.location.toString();
+	var previousData = urlData.substring(urlData.lastIndexOf('?')+1);
+	alert(previousData);
 });
