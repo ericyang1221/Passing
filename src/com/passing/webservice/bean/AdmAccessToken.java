a8c6f79f6c4a9b3fe219a87f4fac523b4fbb8c80
lovelease <lovelease@gmail.com>
2012-08-21 18:08:27 +0800

    bing translate access token generator

---
+++ src/com/passing/webservice/bean/AdmAccessToken.java
@@ -0,0 +1,46 @@
+package com.passing.webservice.bean;
+
+public class AdmAccessToken {
+
+	public String access_token;
+	public String token_type;
+	public String expires_in;
+	public String scope;
+	
+	public AdmAccessToken(){
+	}
+	
+	public AdmAccessToken(String access_token, String token_type,
+			String expires_in, String scope) {
+		super();
+		this.access_token = access_token;
+		this.token_type = token_type;
+		this.expires_in = expires_in;
+		this.scope = scope;
+	}
+
+	public String getAccess_token() {
+		return access_token;
+	}
+	public void setAccess_token(String access_token) {
+		this.access_token = access_token;
+	}
+	public String getToken_type() {
+		return token_type;
+	}
+	public void setToken_type(String token_type) {
+		this.token_type = token_type;
+	}
+	public String getExpires_in() {
+		return expires_in;
+	}
+	public void setExpires_in(String expires_in) {
+		this.expires_in = expires_in;
+	}
+	public String getScope() {
+		return scope;
+	}
+	public void setScope(String scope) {
+		this.scope = scope;
+	}
+}
